5486953593ad876586b6fbc7c8f3a07472044444
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2022-03-07 14:47:50 -0300

    feat: adjustments Home & Contdown implemented

--- src/components/Timer-container/index.tsx
+++ src/components/Timer-container/index.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Styled from './styles';
 
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 25 * 60 // 25 MINUTES
+
 const TimerContainer = () => {
+
+    const [secondsAmount, setSecondsAmount] = useState(0);
+
+    useEffect(() => {
+      if (secondsAmount === 0) {
+          alert('Chegou ao fim!')
+          return;
+      }
+
+      setTimeout(() => {
+          setSecondsAmount(state => state - 1);
+      }, 1000)
+    }, [secondsAmount]);
+
+    const minutes = Math.floor(secondsAmount / 60);
+    const seconds = secondsAmount % 60;
+
+    function StartCountdown() {
+        setSecondsAmount(COUNTDOWN_INITIAL_TIME_IN_SECONDS)
+    }
+
+    function ResetCountdown() {
+        setSecondsAmount(COUNTDOWN_INITIAL_TIME_IN_SECONDS)
+    }
+
     return(
         <Styled.Container>
             <Styled.Wrapper>
-                <Styled.Text>25</Styled.Text>
-                <Styled.SubText>:00</Styled.SubText>
+                <Styled.Hour>{String(minutes).padStart(2, '0')}</Styled.Hour>
+                <Styled.SeparatorTime>:</Styled.SeparatorTime>
+                <Styled.Minutes>{String(seconds).padStart(2, '0')}</Styled.Minutes>
             </Styled.Wrapper>
+
+            <Styled.ButtonContainer>
+                <Styled.ButtonWrapper onPress={() => StartCountdown()}>
+                    <Styled.Button>Start</Styled.Button>
+                </Styled.ButtonWrapper>
+
+                <Styled.ResetButtonWrapper onPress={() => ResetCountdown()}>
+                    <Styled.ResetButton>Reset</Styled.ResetButton>
+                </Styled.ResetButtonWrapper>
+            </Styled.ButtonContainer>
+          
         </Styled.Container>
     )
 }
--- src/components/Timer-container/styles.ts
+++ src/components/Timer-container/styles.ts
@@ -14,16 +14,63 @@ export const Wrapper = styled.View`
     align-items: center;
     border-width: 2px;
     border-radius: 150px;
-    /* box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.5); */
 `;
 
-export const Text = styled.Text`
+export const Hour = styled.Text`
     font-size: 35px;
     font-weight: bold;
     color: #000000;
 `;
-export const SubText = styled.Text`
+
+export const SeparatorTime = styled.Text`
+     font-size: 35px;
+    font-weight: bold;
+    color: #000000;
+    margin-left: 5px;
+    margin-right: 5px;
+`;
+export const Minutes = styled.Text`
  font-size: 35px;
     font-weight: bold;
     color: #000000;
+`;
+
+export const ButtonContainer = styled.View`
+    flex-direction: row;
+    margin-top: 50px;
+`;
+export const ButtonWrapper = styled.TouchableOpacity`
+    width: 125px;
+    height: 50px;
+    background-color: #000000;
+   margin-top: 35px;
+   justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+
+`;
+
+export const Button = styled.Text`
+    color: #FFFFFF;
+    font-size: 25px;
+    font-weight: bold;
+`;
+
+export const ResetButtonWrapper = styled.TouchableOpacity`
+    width: 125px;
+    height: 50px;
+    background-color: #FFFFFF;
+    margin-top: 35px;
+    justify-content: center;
+    align-items: center;
+    margin-left: 25px;
+
+    border-width: 1px;
+    border-radius: 5px;
+`;
+
+export const ResetButton = styled.Text`
+     color: #000000;
+    font-size: 25px;
+    font-weight: bold;
 `;
